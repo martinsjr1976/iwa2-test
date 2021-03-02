e2909e98e16635294b97ae74f60bd68f96cc67f2
martinsjr1976 <martinsjr1976@gmail.com>
2021-03-02 10:18:28 +0000

    users

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const http = require('http');
+const axios = require('axios');
 
 http.createServer((req, res)=>{
-  res.write("Hello world \n"); // write a response
+  res.write(users.join("\n")); //display the list of users on the page
+  res.write(emails.join("\n")); //display the list of emails on the page
   res.end(); //end the response
 }).listen(8000); // listen for requests on port 8000
+
+let users = []; // names of users will be stored here
+let email = [];
+(async function getNames(){
+  try{
+    const {data} = await axios.get("https://jsonplaceholder.typicode.com/users");
+    users = data.map(user=>user.name);
+    emails = data.map(email=>email.email);
+    console.log(users, emails)
+  } catch(error){
+    console.log(error)
+  }
+})()
