12dd89aeaa662070136db4208f31fa19d24beb3d
martinsjr1976 <martinsjr1976@gmail.com>
2021-03-09 11:06:07 +0000

    routing

--- index.js
+++ index.js
@@ -9,18 +9,8 @@ var app = express();
 var port = 8000;
 
 app.use(bodyParser.json())
-
-app.get('/hello/:foo/:bar', (req, res) => {
-    res.json({message: 'Hello BScBest!', data: [
-        req.params.foo,
-        req.params.bar
-    ]});
-});
-
-
-app.post('/hello', (req, res) => {
-    res.json({result: 'Post was sent', data: req.body});
-});
+app.use(logger('tiny'));
+app.use(require('./routes'));
 
 
 // http.createServer((req, res)=>{
