fa7e2bcfa6b2f81980e37e714a3c4293e62ec068
martinsjr1976 <martinsjr1976@gmail.com>
2021-04-04 08:22:12 +0000

    test

--- item-controller.js
+++ item-controller.js
@@ -1,5 +1,9 @@
 exports.getWorld = function(req,res){
-    res.json({result: 'Hello Wrold from Controller'});
+    res.json({result: 'Hello World from Controller'});
+}
+
+exports.getHola = function(req,res){
+    res.json({result: 'Hola from the Controler'});
 }
 
 exports.getWorldParams = function(req,res){
--- routes.js
+++ routes.js
@@ -5,11 +5,10 @@ var itemCtrl = require('./item-controller'),
 userCtrl = require('./user-controller');
 
 router.get('/hello', itemCtrl.getWorld);
-
+router.get('/hola', itemCtrl.getHola);
 router.get('/hello/:foo/:bar', itemCtrl.getWorldParams);
 
 router.post('/hello', itemCtrl.postWorld);
-
 router.post('/users', userCtrl.createUser);
 router.get('/users', userCtrl.getUsers);
 router.get('/users/:id', userCtrl.getUser);
